986afc2a1ac9460bdb889874a283ccbf0a53cbe6
Nikita Veselov <veselovnk@txix.ru>
2025-07-09 17:45:19 +0300

    fix: incorrect display of past events

--- src/utils/getTileXAndWidth.ts
+++ src/utils/getTileXAndWidth.ts
@@ -2,84 +2,38 @@ import { dayWidth, minutesInHour, singleDayWidth, zoom2ColumnWidth } from "@/con
 import { DatesRange } from "./getDatesRange";
 
 export const getTileXAndWidth = (item: DatesRange, range: DatesRange, zoom: number) => {
-  let cellWidth: number;
-  switch (zoom) {
-    case 0:
-      cellWidth = singleDayWidth;
-      break;
-    case 2:
-      cellWidth = zoom2ColumnWidth;
-      break;
-    default:
-      cellWidth = dayWidth;
+  const cellWidth = zoom === 0 ? singleDayWidth : zoom === 2 ? zoom2ColumnWidth : dayWidth;
+
+  const visibleStart = item.startDate.isBefore(range.startDate) ? range.startDate : item.startDate;
+
+  const visibleEnd = item.endDate.isAfter(range.endDate) ? range.endDate : item.endDate;
+
+  if (visibleStart.isSameOrAfter(visibleEnd)) {
+    return { x: 0, width: 0 };
   }
-  const getX = () => {
-    let position;
-    switch (zoom) {
-      case 2:
-        position =
-          (item.startDate.diff(range.startDate, "minute") / minutesInHour + 1) * cellWidth -
-          cellWidth / 2;
-        break;
-      default: {
-        position = (item.startDate.diff(range.startDate, "day") + 1) * cellWidth;
-      }
-    }
-    return Math.max(0, position);
+
+  let x = 0;
+  let width = 0;
+
+  if (zoom === 2) {
+    const minutesFromStart = visibleStart.diff(range.startDate, "minute");
+    const durationMinutes = visibleEnd.diff(visibleStart, "minute");
+
+    x = (minutesFromStart / minutesInHour + 1) * cellWidth - cellWidth / 2;
+    width = (durationMinutes / minutesInHour) * cellWidth;
+  } else {
+    const dayDiff = visibleStart.diff(range.startDate, "day");
+
+    const fullDays = visibleEnd.startOf("day").diff(visibleStart.startOf("day"), "day");
+    const endsLaterInDay = visibleEnd.diff(visibleEnd.clone().startOf("day"), "minute") > 0;
+    const totalDays = fullDays + (endsLaterInDay ? 1 : 0);
+
+    x = dayDiff * cellWidth;
+    width = totalDays * cellWidth;
+  }
+
+  return {
+    x: Math.max(0, x),
+    width
   };
-
-  if (item.startDate.isAfter(range.startDate) && item.endDate.isBefore(range.endDate)) {
-    let width;
-    switch (zoom) {
-      case 2:
-        width = (item.endDate.diff(item.startDate, "minute") / minutesInHour) * cellWidth;
-        break;
-      default:
-        width = item.endDate.diff(item.startDate, "day") * cellWidth + cellWidth;
-    }
-
-    return { x: getX(), width };
-  }
-
-  if (item.startDate.isBefore(range.startDate) && item.endDate.isBefore(range.endDate)) {
-    let width;
-    switch (zoom) {
-      case 2:
-        width =
-          (item.endDate.diff(range.startDate, "minute") / minutesInHour) * cellWidth +
-          0.5 * cellWidth;
-        break;
-      default:
-        width = item.endDate.diff(range.startDate, "day") * cellWidth + cellWidth;
-    }
-
-    return { x: getX(), width };
-  }
-
-  if (item.startDate.isAfter(range.startDate) && item.endDate.isAfter(range.endDate)) {
-    let width;
-    switch (zoom) {
-      case 2:
-        width = (range.endDate.diff(item.startDate, "minute") / minutesInHour) * cellWidth;
-        break;
-      default:
-        width = range.endDate.diff(item.startDate, "day") * cellWidth + cellWidth;
-    }
-
-    return { x: getX(), width };
-  }
-
-  if (item.startDate.isBefore(range.startDate) && item.endDate.isAfter(range.endDate)) {
-    let width;
-    switch (zoom) {
-      case 2:
-        width = (range.endDate.diff(range.startDate, "minute") / minutesInHour) * cellWidth;
-        break;
-      default:
-        width = range.endDate.diff(range.startDate, "day") * cellWidth + cellWidth;
-    }
-
-    return { x: getX(), width };
-  }
-  return { x: getX(), width: 0 };
 };
